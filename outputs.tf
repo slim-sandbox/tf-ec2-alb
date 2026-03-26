ce7fb53aff929df3c2fa97f00c53d1fc8ff111f7
Jesstern Rays <jsstrn@users.noreply.github.com>
2026-03-26 11:17:41 +0800

    feat: add output to see if selected instance type is free tier eligible

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "application_http_url" {
   value = "http://${module.alb.dns_name}"
 }
+
+output "instance_type" {
+  value = "The instance type ${data.aws_ec2_instance_type.selected.instance_type} is ${data.aws_ec2_instance_type.selected.free_tier_eligible ? "" : "not "}free tier eligible."
+}
